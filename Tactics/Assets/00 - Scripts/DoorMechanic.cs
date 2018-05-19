63fb470126c72819192bdd607fefdc69aff0ddcd
DESKTOP-EEIMSU0\K <kentyn1998@gmail.com>
2018-05-19 17:48:35 -0500

    UI + door
    Added Controls UI and got the door mechanic working.

--- Tactics/Assets/00 - Scripts/DoorMechanic.cs
+++ Tactics/Assets/00 - Scripts/DoorMechanic.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorMechanic : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Animator animator;
+    private void OnTriggerEnter(Collider other)
+    {
+      
+        if (other.gameObject.tag == "Player" && KeyItem.keyCount >= 1)
+        {
+            animator.SetBool("character_nearby", true);
+            KeyItem.keyCount--;
+        }
+        else
+        {
+            animator.SetBool("character_nearby", false); 
+        }
+    }
 }
