02bb74da462c15dc130344cb6f718c11eff47a4b
DESKTOP-EEIMSU0\K <kentyn1998@gmail.com>
2018-05-28 21:37:24 -0500

    Working on Audio
    PickUp item Audio not working correctly, door Audio needs a new one + close

--- Tactics/Assets/00 - Scripts/DoorMechanic.cs
+++ Tactics/Assets/00 - Scripts/DoorMechanic.cs
@@ -5,21 +5,32 @@ using UnityEngine;
 public class DoorMechanic : MonoBehaviour {
     public Animator animator;
     public Light lt;
- 
+    public AudioClip GreenMusicClip;
+    public AudioSource GreenMusicSource;
+    public AudioClip RedMusicClip;
+    public AudioSource RedMusicSource;
+    public AudioClip DoorMusicClip;
+    public AudioSource DoorMusicSource;
+
     private void OnTriggerEnter(Collider other)
     {
-      
+        GreenMusicSource.clip = GreenMusicClip;
+        RedMusicSource.clip = RedMusicClip;
+        DoorMusicSource.clip = DoorMusicClip;
         if (other.gameObject.tag == "Player" && KeyItem.keyCount >= 1)
         {
             
             animator.SetBool("character_nearby", true);
             lt.color = Color.green;
+            GreenMusicSource.Play();
             KeyItem.keyCount--;
+            DoorMusicSource.Play();
         }
         else
         {
             animator.SetBool("character_nearby", false);
             lt.color = Color.red;
+            RedMusicSource.Play();
         }
     }
 
--- Tactics/Assets/00 - Scripts/KeyItem.cs
+++ Tactics/Assets/00 - Scripts/KeyItem.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class KeyItem : MonoBehaviour {
     public static int keyCount;
+    public AudioClip MusicClip;
+    public AudioSource MusicSource;
     void OnTriggerEnter (Collider other)
     {
+        MusicSource.clip = MusicClip;
         if (other.gameObject.tag == "Player")
         {
             keyCount += 1;
+            MusicSource.Play();
             Destroy(gameObject);
         }
     }
